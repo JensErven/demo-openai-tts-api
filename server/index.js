274d81d34dea05357980284d1cd02f7858172e2d
Jens Erven <jens.erven@student.ehb.be>
2023-11-28 15:53:51 +0100

    bugfix: change code to use fs and path module

--- server/index.js
+++ server/index.js
@@ -3,13 +3,14 @@ const app = express(); // Use express() to create the app
 
 const server = require("http").createServer(app);
 const cors = require("cors");
-const openai = require("openai");
+const OpenAI = require("openai");
 require("dotenv").config();
 const fs = require("fs");
+const path = require("path");
 
 const apiKey = process.env.REACT_APP_OPENAI_API_KEY;
 
-const openaiClient = new openai.OpenAI({ apiKey });
+const openai = new OpenAI({ apiKey });
 
 const allowedOrigins = [
   "http://localhost:3000",
@@ -34,13 +35,21 @@ app.get("/", (req, res) => {
 app.post("/generate-speech", async (req, res) => {
   const { text, selectedVoiceModel } = req.body; // Assuming text is sent in the request body
   try {
-    const mp3 = await openaiClient.audio.speech.create({
+    const mp3 = await openai.audio.speech.create({
       model: "tts-1",
       voice: selectedVoiceModel,
       input: text,
     });
-    const audioBuffer = Buffer.from(await mp3.buffer());
-    res.send(audioBuffer);
+
+    const audioBuffer = Buffer.from(await mp3.arrayBuffer());
+
+    // Generating a unique file name based on timestamp for each speech request
+    const speechFile = path.resolve(`./speech-${Date.now()}.mp3`);
+
+    // Writing the audio buffer to a file
+    await fs.promises.writeFile(speechFile, audioBuffer);
+
+    res.sendFile(speechFile);
   } catch (error) {
     console.error("Error generating speech:", error);
     res.status(500).send("Speech generation failed");
